9a85f465e0176171d7bb75ed776f8a8b44c1cc99
owen07020 <153528559+owen07020@users.noreply.github.com>
2025-03-29 12:37:13 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import ProductProfile from './pages/ProductProfile';
 import MasterStockTake from './pages/MasterStockTake';
 
@@ -13,14 +13,21 @@ function App() {
   return (
     <Router>
       <div className="flex">
+        {/* Sidebar */}
         <div className="w-1/4 bg-gray-100 min-h-screen p-4">
-          <h1 className="text-xl font-bold mb-4">Cream Calculator</h1>
-          <ul>
-            <li><a href="/" className="text-blue-600">Product Profile</a></li>
-            <li><a href="/mst" className="text-blue-600">Master Stock Take</a></li>
+          <h1 className="text-xl font-bold mb-6">Cream Calculator</h1>
+          <ul className="space-y-2">
+            <li>
+              <Link to="/" className="text-blue-600 hover:underline">Product Profile</Link>
+            </li>
+            <li>
+              <Link to="/mst" className="text-blue-600 hover:underline">Master Stock Take</Link>
+            </li>
           </ul>
         </div>
-        <div className="w-3/4 p-4">
+
+        {/* Main Page */}
+        <div className="w-3/4 p-6">
           <Routes>
             <Route path="/" element={<ProductProfile onSave={handleSaveProfile} />} />
             <Route path="/mst" element={<MasterStockTake profiles={profiles} />} />
